d6274f6a305795afeb67252fd1c0dc2fe3a4fff4
guest <kamil.topolewski@gmail.com>
2019-03-04 12:39:09 +0100

    Final touches to the app

--- app/src/main/java/com/example/bookatreat/MainActivity.java
+++ app/src/main/java/com/example/bookatreat/MainActivity.java
@@ -5,6 +5,10 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    /**
+     * That's a method man
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
