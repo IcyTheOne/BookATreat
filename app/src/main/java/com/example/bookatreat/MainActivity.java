21e78dc7c3d705ed50314746028282fad438e2b8
IcyTheOne <alexanderbjerre@hotmail.com>
2019-03-21 13:14:20 +0100

    imported firebase packages and fixed google services.json file

--- app/src/main/java/com/example/bookatreat/MainActivity.java
+++ app/src/main/java/com/example/bookatreat/MainActivity.java
@@ -3,7 +3,11 @@ package com.example.bookatreat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.firestore.FirebaseFirestore;
+
 public class MainActivity extends AppCompatActivity {
+
+
 
     /**
      * That's a nice method man test push floobits testefefefef
