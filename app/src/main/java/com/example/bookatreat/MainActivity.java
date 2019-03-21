714eb8e7fcf7011adb99a7d040004db02ff0259f
IcyTheOne <alexanderbjerre@hotmail.com>
2019-03-21 14:43:05 +0100

    Added firebase authentication instance

--- app/src/main/java/com/example/bookatreat/MainActivity.java
+++ app/src/main/java/com/example/bookatreat/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.bookatreat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,11 +14,14 @@ public class MainActivity extends AppCompatActivity {
      * That's a nice method man test push floobits testefefefef
      * @param savedInstanceState
      */
-
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+
     }
 }
 
