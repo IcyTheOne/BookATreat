e19cc63fe37f89f3fac572a9f14c802008015a5c
bruger <1234>
2019-05-24 16:39:40 +0200

    add item to message frag

--- app/src/main/java/com/example/bookatreat/Customer/MessageFrag.java
+++ app/src/main/java/com/example/bookatreat/Customer/MessageFrag.java
@@ -8,8 +8,10 @@ import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import com.example.bookatreat.R;
 
@@ -18,7 +20,7 @@ import java.util.ArrayList;
 public class MessageFrag extends Fragment {
 
 
-    private ArrayList<MessageFrag> MessageList;
+    private ArrayList<String> MessageArr;
 
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_message, container, false);
@@ -26,20 +28,25 @@ public class MessageFrag extends Fragment {
         // Find IDs
         ImageButton settingsButton = view.findViewById(R.id.SettingsBTN);
 
-
-        ListView messageButton = view.findViewById(R.id.MessagesList);
-
-
-
-        MessageList = new ArrayList<>();
-
-        //MessageList.add();
+        ListView messagesList = view.findViewById(R.id.MessagesList);
 
 
 
+        MessageArr = new ArrayList<>();
+
+        MessageArr.add("TGI FRIDAY, CONFIRMATION");
 
 
+        ArrayAdapter adapter = new ArrayAdapter(getContext(), android.R.layout.simple_list_item_1, MessageArr);
+        messagesList.setAdapter(adapter);
 
+
+ /*       public void onItemClick(AdapterView <? > arg0, View arg1, int arg2,
+        long arg3) {
+            Log.v("clicked", (String)((TextView) arg1).getText());
+        }
+
+    });*/
 
 
 
