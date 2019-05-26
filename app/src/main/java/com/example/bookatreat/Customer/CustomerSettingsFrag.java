1ac0dda500e50275eaf004b524c3fe788b976b96
IcyTheOne <alexanderbjerre@hotmail.com>
2019-05-26 13:09:23 +0200

    Update with customers settings, doing design now so it looks as it should for final version and then for restaurant

--- app/src/main/java/com/example/bookatreat/Customer/CustomerSettingsFrag.java
+++ app/src/main/java/com/example/bookatreat/Customer/CustomerSettingsFrag.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -150,7 +152,10 @@ public class CustomerSettingsFrag extends Fragment {
             }
         });
 
-        //mEditBTN.setOnClickListener();
+        //mEditBTN.
+
+
+
 
 
 
