f86a4c2fec141442b58a8dc356ac943807bad0c4
guest <kamil.topolewski@gmail.com>
2019-05-26 13:49:40 +0200

    CustomerEditSetting xml

--- app/src/main/java/com/example/bookatreat/Customer/CustomerActivity.java
+++ app/src/main/java/com/example/bookatreat/Customer/CustomerActivity.java
@@ -1,13 +1,11 @@
 package com.example.bookatreat.Customer;
 
-import android.support.annotation.NonNull;
 import android.support.v4.app.FragmentTransaction;
 import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
 
