5ffd81ee027bb25287d0bd8f40ab68e3f9c3bac3
uezenmud <44086734+uezenmud@users.noreply.github.com>
2019-05-26 19:06:27 +0200

    map view

--- app/src/main/java/com/example/bookatreat/Customer/MapsViewFrag.java
+++ app/src/main/java/com/example/bookatreat/Customer/MapsViewFrag.java
@@ -11,13 +11,41 @@ import android.view.ViewGroup;
 import android.widget.ImageButton;
 
 import com.example.bookatreat.R;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import static com.example.bookatreat.Constants.MAPVIEW_BUNDLE_KEY;
 
 
-public class MapsViewFrag extends Fragment {
+public class MapsViewFrag extends Fragment implements OnMapReadyCallback {
+
+    public MapView mMapView;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_maps_view, container, false);
         // Find IDs
         ImageButton settingsButton = view.findViewById(R.id.SettingsBTN);
+        mMapView = view.findViewById(R.id.map_view);
+
+        // Initiate the MapView Object
+//        initGoogleMaps(savedInstanceState);
+
+        Bundle mapViewBundle = null;
+        if (savedInstanceState != null) {
+            mapViewBundle = savedInstanceState.getBundle(MAPVIEW_BUNDLE_KEY);
+        }
+        mMapView.onCreate(mapViewBundle);
+
+        mMapView.getMapAsync(this);
 
         // Go to Settings
         settingsButton.setOnClickListener(new View. OnClickListener() {
@@ -30,5 +58,71 @@ public class MapsViewFrag extends Fragment {
             }
         });
         return view;
+    }
+//    private void initGoogleMaps(Bundle savedInstanceState){
+//        // *** IMPORTANT ***
+//        // MapView requires that the Bundle you pass contain _ONLY_ MapView SDK
+//        // objects or sub-Bundles.
+//        Bundle mapViewBundle = null;
+//        if (savedInstanceState != null) {
+//            mapViewBundle = savedInstanceState.getBundle(MAPVIEW_BUNDLE_KEY);
+//        }
+//        mMapView.onCreate(mapViewBundle);
+//
+//        mMapView.getMapAsync(this);
+//    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        Bundle mapViewBundle = outState.getBundle(MAPVIEW_BUNDLE_KEY);
+        if (mapViewBundle == null) {
+            mapViewBundle = new Bundle();
+            outState.putBundle(MAPVIEW_BUNDLE_KEY, mapViewBundle);
+        }
+
+        mMapView.onSaveInstanceState(mapViewBundle);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mMapView.onResume();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mMapView.onStart();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        mMapView.onStop();
+    }
+
+    @Override
+    public void onMapReady(GoogleMap map) {
+        map.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
+    }
+
+    @Override
+    public void onPause() {
+        mMapView.onPause();
+        super.onPause();
+    }
+
+    @Override
+    public void onDestroy() {
+        mMapView.onDestroy();
+        super.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mMapView.onLowMemory();
     }
 }
